8639fcfa43368f0ce2d5221800e8f0787e48ebc9
Taylor Gagne <taylor@baremetalcloud.com>
2021-06-03 03:41:09 -0400

    First Commit

---
+++ Source/Building_Escape/WorldPosition.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldPosition.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Display, TEXT("This is info"));
+
+	UE_LOG(LogTemp, Warning, TEXT("This is a Warning!"));
+
+	UE_LOG(LogTemp, Error, TEXT("This is an Error!!"));
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
